ef468092740547a51e49c37e8615d8946fa4d9b3
Heath Stewart <heaths@microsoft.com>
2022-07-05 22:35:28 -0700

    Use git repo as title

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -38,6 +38,11 @@ function global:prompt {
 # Increase history count.
 $global:MaximumHistoryCount = 100
 
+# Remember original title.
+if (!(test-path variable:\Profile_OriginalTitle)) {
+    new-variable Profile_OriginalTitle -option Constant -visibility Private -value ([Console]::Title)
+}
+
 # Private functions
 
 if (!(test-path variable:\Profile_GetBranch)) {
@@ -45,7 +50,11 @@ new-variable Profile_GetBranch -option Constant -visibility Private -value {
 
     if ($dir = &$Profile_GetRepo) {
 
-        $repo = new-object PSObject -property @{ 'Branch' = $null; 'SCM' = $dir.SCM }
+        $repo = new-object PSObject -property @{
+            Branch = $null
+            Clone = $dir.Clone
+            SCM = $dir.SCM
+        }
 
         switch ($dir.SCM) {
             'Git' {
@@ -88,22 +97,28 @@ new-variable Profile_GetRepo -option Constant -visibility Private -value {
 
         if (($gd = join-path $dir '.git') -and (test-path $gd)) {
             if (test-path $gd -pathtype 'Container') {
-                # check if git repository
-                return (resolve-path $gd | add-member -type NoteProperty -name 'SCM' -value 'Git' -passthru)
+                # git repository
+                return (resolve-path $gd `
+                    | add-member -type NoteProperty -name 'SCM' -value 'Git' -passthru `
+                    | add-member -type NoteProperty -name 'Clone' -value (join-path $gd .. -resolve | split-path -leaf) -passthru)
 
             } elseif (test-path $gd -pathtype 'Leaf') {
-                # check if git submodule
+                # git submodule or worktree
                 if ((resolve-path $gd | get-content) -match 'gitdir: (?<d>.+)') {
                     # join-path simply concatenates both paths so use path.combine if gitdir is absolute
                     if (($gd = [io.path]::combine($dir, $Matches['d'])) -and (test-path $gd -pathtype 'Container')) {
-                        return (resolve-path $gd | add-member -type NoteProperty -name 'SCM' -value 'Git' -passthru)
+                        return (resolve-path $gd `
+                            | add-member -type NoteProperty -name 'SCM' -value 'Git' -passthru `
+                            | add-member -type NoteProperty -name 'Clone' -value ($gd | split-path -leaf) -passthru)
                     }
                 }
             }
 
         } elseif (($gd = join-path $dir '.hg') -and (test-path $gd)) {
             # check if hg repository
-            return (resolve-path $gd | add-member -type NoteProperty -name 'SCM' -value 'Hg' -passthru)
+            return (resolve-path $gd `
+                | add-member -type NoteProperty -name 'SCM' -value 'Hg' -passthru `
+                | add-member -type NoteProperty -name 'Clone' -value (join-path $gd .. -resolve | split-path -leaf) -passthru)
         }
     }
 }}
@@ -190,7 +205,10 @@ new-variable Profile_Prompt -option Constant -visibility Private -value $(
         }, $Profile_Colors.WHITE, $Profile_Colors.LIGHTGRAY}
         {"`n"}
         {if ($repo = &$Profile_GetBranch -and $repo.Branch) {
+            [Console]::Title = $repo.Clone
             (" $($Profile_Chars.BRANCH) $($repo.Branch) "), $Profile_Colors.WHITE, $Profile_Colors.DARKGRAY
+        } else {
+            [Console]::Title = $Profile_OriginalTitle
         }}
         {if ($c = $global:ExecutionContext.SessionState.Path.LocationStack($null).Count) {
             (' ' + '+' * $c), $Profile_Colors.LIGHTERGRAY, $Profile_Colors.LIGHTGRAY
@@ -279,6 +297,7 @@ new-variable Profile_FormatPrompt -option Constant -visibility Private -value {
 if (!(test-path variable:\Git)) {
 new-object PSObject | `
     add-member -name Branch -type ScriptProperty -value { (&$Profile_GetBranch).Branch } -passthru | `
+    add-member -name Clone -type ScriptProperty -value { (&$Profile_GetRepo).Clone } -passthru | `
     add-member -name Root -type ScriptProperty -value { (&$Profile_GetRepo).Path } -passthru | `
     new-variable -name Git -option Constant -scope Global
 }
